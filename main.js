c0384e7c2db8976d233bdd6aff2afa40db4c0cdd
adepunandhu9@gmail.com <adepunandhu9@gmail.com>
2023-03-08 22:46:56 +0530

    download not working

--- main.js
+++ main.js
@@ -1,32 +1,63 @@
 let name = prompt("What is your name?");
 
-let botoutput = document.getElementById("botoutput");
+
+/*Image headings */
 let ideoutput = document.getElementById("ideoutput");
+let botoutput = document.getElementById("botoutput");
 let pixoutput = document.getElementById("pixoutput");
 let adoutput = document.getElementById("adoutput");
 let thumboutput = document.getElementById("thumboutput");
 let iconoutput = document.getElementById("iconoutput");
 
 
-let  format  = ["Botts","Identicon","Pixel-art","Adventure","Thumb",'Icon'];
-let paraItmes = [botoutput,ideoutput,pixoutput,adoutput,thumboutput,iconoutput];
+/* Images */
+let identicon = document.getElementById('identicon');
+let bot = document.getElementById('bot');
+let pixel = document.getElementById('pixel');
+let adventure = document.getElementById('adventure');
+let thumb = document.getElementById('thumb');
+let icon = document.getElementById('icon');
 
-paraItmes.forEach((e, i) => {
-  e.textContent = `Your name ${name} in ${format[i]} Image format`;
-});
+/* download links */
+let identiconLink = document.getElementById('identicon-link');
+let botLink = document.getElementById('bot-link');
+let pixelLink = document.getElementById('pixel-link');
+let adventureLink = document.getElementById('adventure-link');
+let thumbLink = document.getElementById('thumb-link');
+let iconLink = document.getElementById('icon-link');
+
+
+let  format  = ["Identicon","Botts","Pixel-art","Adventure","Thumb",'Icon'];
+let paraItmes = [ideoutput,botoutput,pixoutput,adoutput,thumboutput,iconoutput];
+let Items = [identicon,bot,pixel,adventure,thumb,icon];
+let path = [
+    '5.x/identicon',
+    '5.x/bottts',
+    '4.x/pixel-art',
+    '5.x/adventurer',
+    '5.x/thumbs',
+    '5.x/icons'
+  ];
+let links = [identiconLink,botLink,pixelLink,adventureLink,thumbLink,iconLink];
+
+function makeRequest(){
+  Items.forEach((e, i) => {
+    paraItmes[i].textContent = `Your name ${name} in ${format[i]} Image format`;
+    e.src = "https://api.dicebear.com/" + path[i] + "/svg?seed=" + name;
+    links[i].href = "https://api.dicebear.com/" + path[i] + "/svg?seed=" + name;
+  });
+}
+
+if(name !== null || name !== undefined){
+  makeRequest();
+}else{
+  window.location.reload();
+}
 
 
 
-document.getElementById("bot").src = "https://api.dicebear.com/5.x/bottts/svg?seed=" + name;
-
-document.getElementById("identicon").src = "https://api.dicebear.com/5.x/identicon/svg?seed=" + name;
-document.getElementById("pixel").src = "https://api.dicebear.com/4.x/pixel-art/svg?seed=" + name;
-document.getElementById("adventure").src = "https://api.dicebear.com/5.x/adventurer/svg?seed=" + name;
-document.getElementById('thumb').src = "https://api.dicebear.com/5.x/thumbs/svg?seed=" + name;
-document.getElementById('icon').src = "https://api.dicebear.com/5.x/icons/svg?seed=" + name;
-
+/*Relaod*/
 let refresh = document.getElementById('note');
-
 refresh.addEventListener('click',() => {
   window.location.reload();
 });
