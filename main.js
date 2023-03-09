2512415c3d369dc410ff6fe3ffd71ed3e90126a2
adepunandhu9@gmail.com <adepunandhu9@gmail.com>
2023-03-09 11:52:50 +0530

    hshvbdd

--- main.js
+++ main.js
@@ -44,8 +44,8 @@ let links = [identiconLink,botLink,pixelLink,adventureLink,thumbLink,iconLink];
 async function makeRequest(){
   Items.forEach(async (e, i) => {
     paraItmes[i].innerHTML = `Your name <b>${name}</b> in <b>${format[i]}</b> Image format`;
-    e.src = `https://api.dicebear.com/${path[i]}/svg?seed=${name}`;
-    let response = await fetch(`https://api.dicebear.com/${path[i]}/svg?seed=${name}`);
+    e.src = `https://api.dicebear.com/${path[i]}/jpg?seed=${name}`;
+    let response = await fetch(`https://api.dicebear.com/${path[i]}/jpg?seed=${name}`);
     let blob = await response.blob();
     const imageUrl = URL.createObjectURL(blob);
     links[i].href = imageUrl;
