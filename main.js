3752ad948e36ca77967c90b50d6678ead48f722b
adepunandhu9@gmail.com <adepunandhu9@gmail.com>
2023-03-16 13:36:23 +0530

    jdjjdjdd

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const nameInput = document.getElementById('name-input');
 const nameForm = document.getElementById("nameForm");
 
@@ -6,34 +5,7 @@ const nameForm = document.getElementById("nameForm");
 let imagesContainer = document.getElementById('container');
 imagesContainer.classList.add('hide');
 
-/*Image headings */
-let ideoutput = document.getElementById("ideoutput");
-let botoutput = document.getElementById("botoutput");
-let pixoutput = document.getElementById("pixoutput");
-let adoutput = document.getElementById("adoutput");
-let thumboutput = document.getElementById("thumboutput");
-let iconoutput = document.getElementById("iconoutput");
-
-/* Images */
-let identicon = document.getElementById('identicon');
-let bot = document.getElementById('bot');
-let pixel = document.getElementById('pixel');
-let adventure = document.getElementById('adventure');
-let thumb = document.getElementById('thumb');
-let icon = document.getElementById('icon');
-
-/* download downloadButtons */
-let identiconLink = document.getElementById('identicon-link');
-let botLink = document.getElementById('bot-link');
-let pixelLink = document.getElementById('pixel-link');
-let adventureLink = document.getElementById('adventure-link');
-let thumbLink = document.getElementById('thumb-link');
-let iconLink = document.getElementById('icon-link');
-
-/* Arrays for manipulation */
-let formatLabels  = ["Identicon","Botts","Pixel-art","Adventure","Thumb",'Icon'];
-let paraItmes = [ideoutput,botoutput,pixoutput,adoutput,thumboutput,iconoutput];
-let imagesAll = [identicon,bot,pixel,adventure,thumb,icon];
+let formats  = ["Identicon","Botts","Pixel-art","Adventure","Thumb",'Icon'];
 let path = [
     '5.x/identicon',
     '5.x/bottts',
@@ -43,29 +15,51 @@ let path = [
     '5.x/icons'
   ];
   
-let downloadButtons = [identiconLink,botLink,pixelLink,adventureLink,thumbLink,iconLink];
-downloadButtons.forEach((button, i) => {
-  downloadButtons[i].style.opacity = '0.6';
-  downloadButtons[i].textContent = "Loading...";
-});
+async function doWork(string){
+  //For removing previously created child's
+  imagesContainer.innerHTML = '';
+  
+  formats.forEach(async (e, i) => {
+    
+    //creating elements
+    let imageDiv = document.createElement('div');
+    let paraTag = document.createElement('p');
+    let imageTag = document.createElement('img');
+    let aButton = document.createElement('a');
+    
+    //Adding attributes and values
+    imageDiv.classList.add('imageDiv');
+    
+    paraTag.innerHTML = `Your name <b>${string}</b> in <b>${formats[i]}</b> Image formats`;
+    
+    imageTag.src = `https://api.dicebear.com/${path[i]}/svg?seed=${string}`;
 
-/* Making and adding appropriate values */
-async function makeRequest(){
-  imagesAll.forEach(async (image, i) => {
-    paraItmes[i].innerHTML = `Your name <b>${nameInput.value}</b> in <b>${formatLabels[i]}</b> Image formatLabels`;
-    image.src = `https://api.dicebear.com/${path[i]}/svg?seed=${nameInput.value}`;
-    let response = await fetch(`https://api.dicebear.com/${path[i]}/jpg?seed=${nameInput.value}`);
+    aButton.classList.add('download');
+    aButton.download = `${string}.jpg`;
+   
+    // Making request for Download button
+    let response = await fetch(`https://api.dicebear.com/${path[i]}/jpg?seed=${string}`);
     let blob = await response.blob();
     const imageUrl = URL.createObjectURL(blob);
-    downloadButtons[i].href = imageUrl;
-    downloadButtons[i].download = `${nameInput.value}.jpg`;
-    downloadButtons[i].style.opacity = '1';
-    downloadButtons[i].textContent = 'Download';
+    aButton.href = imageUrl;
+    aButton.style.opacity = '1';
+    aButton.textContent = 'Download';
+    
+    //Adding child's 
+    imageDiv.appendChild(paraTag);
+    imageDiv.appendChild(imageTag);
+    imageDiv.appendChild(aButton);
+
+    imagesContainer.appendChild(imageDiv);
+    
   });
 }
 
+
 nameForm.addEventListener('submit',() => {
   event.preventDefault();
-  makeRequest();
-  imagesContainer.classList.remove('hide');
+  doWork(nameInput.value);
+  setTimeout(() => {
+      imagesContainer.classList.remove('hide');
+  },500);
 });
