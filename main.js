30073d98eba3a76702c4087fc424a5be278c3293
adepunandhu9@gmail.com <adepunandhu9@gmail.com>
2023-03-16 09:12:13 +0530

    hzhhss

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-let name = prompt("What is your name?");
+let name = document.getElementById('name');
+let inputContainer = document.getElementById('input-container');
 
+/*
+name.addEventListener('focus',() => {
+  inputContainer.style.border = "2px solid #42445A";
+});*/
 
 /*Image headings */
 let ideoutput = document.getElementById("ideoutput");
@@ -8,7 +13,6 @@ let pixoutput = document.getElementById("pixoutput");
 let adoutput = document.getElementById("adoutput");
 let thumboutput = document.getElementById("thumboutput");
 let iconoutput = document.getElementById("iconoutput");
-
 
 /* Images */
 let identicon = document.getElementById('identicon');
@@ -38,11 +42,17 @@ let path = [
     '5.x/thumbs',
     '5.x/icons'
   ];
+  
 let links = [identiconLink,botLink,pixelLink,adventureLink,thumbLink,iconLink];
+links.forEach((e, i) => {
+  links[i].style.opacity = '0.6';
+  links[i].textContent = "Loading...";
+});
 
 /* Making and adding appropriate values */
 async function makeRequest(){
   Items.forEach(async (e, i) => {
+    links[i].textContent = "Loading...";
     paraItmes[i].innerHTML = `Your name <b>${name}</b> in <b>${format[i]}</b> Image format`;
     e.src = `https://api.dicebear.com/${path[i]}/svg?seed=${name}`;
     let response = await fetch(`https://api.dicebear.com/${path[i]}/jpg?seed=${name}`);
@@ -50,10 +60,16 @@ async function makeRequest(){
     const imageUrl = URL.createObjectURL(blob);
     links[i].href = imageUrl;
     links[i].download = `${name}.jpg`;
+    links[i].style.opacity = '1';
+    links[i].textContent = 'Download';
   });
 }
 
+name.addEventListener('input',{
+  makeRequest();
+});
 
+/*
 if(name !== null && name !== undefined){
   makeRequest();
 }else{
@@ -61,8 +77,9 @@ if(name !== null && name !== undefined){
 }
 
 
-/*Relaod*/
+Relaod
 let refresh = document.getElementById('note');
 refresh.addEventListener('click',() => {
   window.location.reload();
 });
+*/
